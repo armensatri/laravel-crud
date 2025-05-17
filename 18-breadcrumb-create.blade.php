bb85be97b5f162d720fd8be591365a5107a0d199
armensatri <belajarcoding444@gmail.com>
2025-05-17 22:14:56 +0700

    update

---
+++ 18-breadcrumb-create.blade.php
@@ -0,0 +1,13 @@
+<ol class="flex items-center gap-[5px] mb-5 ml-2">
+  <x-breadcrumb-icon
+    image="/backend/img/menu/users.jpg"
+  />
+
+  <x-slash/>
+
+  <x-breadcrumb-name name="users"/>
+
+  <x-slash/>
+
+  <x-breadcrumb-name name="create"/>
+</ol>
---
+++ 18-breadcrumb-edit.blade.php
@@ -0,0 +1,17 @@
+<ol class="flex items-center gap-[5px] mb-5 ml-2">
+  <x-breadcrumb-icon
+    image="/backend/img/menu/users.jpg"
+  />
+
+  <x-slash/>
+
+  <x-breadcrumb-name name="users"/>
+
+  <x-slash/>
+
+  <x-breadcrumb-name :name="$user->username"/>
+
+  <x-slash/>
+
+  <x-breadcrumb-name name="edit"/>
+</ol>
