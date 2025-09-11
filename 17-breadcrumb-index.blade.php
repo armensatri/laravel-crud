35e9bba5807607868ae36f0dbb8ba8ed600e7e77
armensatri <belajarcoding444@gmail.com>
2025-09-11 12:43:20 +0700

    update

--- 17-breadcrumb-index.blade.php
+++ 17-breadcrumb-index.blade.php
@@ -1,15 +1,9 @@
-<ol class="flex items-center gap-[4px] mb-5 ml-2">
+<ol class="flex items-center gap-[5px] mb-5 ml-2">
   <x-breadcrumb-icon
-    image="/backend/img/menu/users.jpg"
+    :image="asset('backend/img/menu/users.jpg')"
   />
 
   <x-slash/>
 
-  <x-breadcrumb-name name="users"/>
-
-  <x-slash/>
-
-  <x-breadcrumb-name :name="'@' . $user->username"
-    class="text-blue-600"
-  />
+  <x-breadcrumb-name name="users" class="text-blue-600"/>
 </ol>
--- 17-breadcrumb-show.blade.php
+++ 17-breadcrumb-show.blade.php
@@ -1,4 +1,4 @@
-<ol class="flex items-center gap-[5px] mb-5 ml-2">
+<ol class="flex items-center gap-[4px] mb-5 ml-2">
   <x-breadcrumb-icon
     image="/backend/img/menu/users.jpg"
   />
@@ -9,5 +9,7 @@
 
   <x-slash/>
 
-  <x-breadcrumb-name :name="$user->username"/>
+  <x-breadcrumb-name :name="'@' . $user->username"
+    class="text-blue-600"
+  />
 </ol>
